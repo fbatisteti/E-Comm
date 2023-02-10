481b6982ab6a1dd8c645a6b706b18ad48d503b43
fbatisteti <fbatisteti@hotmail.com>
2023-02-10 19:35:01 -0300

    ajustes deal of the day

--- ecomm/lib/features/home/widgets/deal_of_the_day.dart
+++ ecomm/lib/features/home/widgets/deal_of_the_day.dart
@@ -6,7 +6,7 @@ import 'package:ecomm/models/product.dart';
 import 'package:flutter/material.dart';
 
 class DealOfTheDay extends StatefulWidget {
-  const DealOfTheDay({ Key? key }) : super(key: key);
+  const DealOfTheDay({Key? key}) : super(key: key);
 
   @override
   _DealOfTheDayState createState() => _DealOfTheDayState();
@@ -28,7 +28,11 @@ class _DealOfTheDayState extends State<DealOfTheDay> {
   }
 
   void navigateToDetailsScreen() {
-    Navigator.pushNamed(context, ProductDetailsScreen.routeName, arguments: product,);
+    Navigator.pushNamed(
+      context,
+      ProductDetailsScreen.routeName,
+      arguments: product,
+    );
   }
 
   void navigateToAllCategoriesPage(BuildContext context) {
@@ -38,83 +42,98 @@ class _DealOfTheDayState extends State<DealOfTheDay> {
   @override
   Widget build(BuildContext context) {
     return (product == null)
-    ? const Loader()
-    : (product!.name == '')
-    ? const SizedBox()
-    : GestureDetector(
-      onTap: navigateToDetailsScreen,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: const Text(
-                    'Deal of the Day',
-                    style: TextStyle(fontSize: 20),),
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: GestureDetector(
-                    onTap: () => navigateToAllCategoriesPage(context),
-                    child: Text(
-                      'See all deals',
-                      style: TextStyle(color: Colors.cyan[800],),
+        ? const Loader()
+        : (product!.name == '')
+            ? const SizedBox()
+            : GestureDetector(
+                onTap: navigateToDetailsScreen,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            alignment: Alignment.topLeft,
+                            child: const Text(
+                              'Deal of the Day',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topLeft,
+                            child: GestureDetector(
+                              onTap: () => navigateToAllCategoriesPage(context),
+                              child: Text(
+                                'See all deals',
+                                style: TextStyle(
+                                  color: Colors.cyan[800],
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
-                )
-              ],
-            ),
-          ),          
-          Image.network(
-            product!.images[0],
-            height: 235,
-            fit: BoxFit.contain,  
-          ),
-          Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.only(left: 15,),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product!.name,
-                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    Image.network(
+                      product!.images[0],
+                      height: 235,
+                      fit: BoxFit.contain,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(
+                        left: 15,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product!.name,
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '\$${product!.price.toString()}',
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding:
+                          const EdgeInsets.only(left: 15, top: 5, right: 40),
+                      child: const Text(
+                        'More images',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: product!.images
+                            .map(
+                              (e) => Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: Image.network(
+                                  e,
+                                  fit: BoxFit.contain,
+                                  width: 100,
+                                  height: 100,
+                                ),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                  ],
                 ),
-                Text(
-                  '\$${product!.price.toString()}',
-                  style: const TextStyle(fontSize: 18,),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            alignment: Alignment.topLeft,
-            padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
-            child: const Text(
-              'More images',
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: product!.images.map((e) => Image.network(
-                  e,
-                  fit: BoxFit.contain,
-                  width: 100,
-                  height: 100,
-                ),
-              ).toList(),
-            ),
-          ),
-        ],
-      ),
-    );
+              );
   }
 }
